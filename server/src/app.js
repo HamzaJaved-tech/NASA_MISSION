8a82dbfb6347b1c088c3c0a6ab36ba5fc8e7b259
Hamza Javed <hamza@efsol.com.pk>
2023-06-25 21:56:37 +0500

    FEAT: Launches modal, getAllLaunches request

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { planetRouter } = require("./routes/planet/planets.router.js");
+const { launchesRouter } = require("./routes/launches/launches.router.js");
 var corsOptions = {
   origin: "*",
 };
@@ -11,6 +12,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(planetRouter);
+app.use(launchesRouter);
+
 app.get("/", (req, res) => {
   return res.send("Welcome to NASA App");
 });
