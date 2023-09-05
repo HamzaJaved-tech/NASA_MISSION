4c1a12520ec7b4ea125c9cea7b1ee4bd6937bb4e
Hamza Javed <hamza.javed@zekiexperts.com>
2023-09-05 16:37:27 +0500

    FEAT: Mongoose setup

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,28 @@
-const http = require('http');
+const http = require("http");
 
-const app = require('./app');
+const app = require("./app");
+const mongoose = require("mongoose");
 
-const { loadPlanetsData } = require('./models/planets.model');
+const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const DB_URL = process.env.DATABASE_URI;
 
 const server = http.createServer(app);
 
+mongoose.connection.on("open", (res) => {
+  console.log("Connected to Mongoose");
+});
+
+mongoose.connection.on("error", (res) => {
+  console.log("Error while connecting to Mongoose");
+});
+
 async function startServer() {
   await loadPlanetsData();
-  
+  await mongoose.connect(
+    "mongodb+srv://graphql:graphql12345@cluster0.sad7oad.mongodb.net/test"
+  );
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
